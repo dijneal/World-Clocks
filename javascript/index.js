62e52b0f8d782d5e59aa32e13f32647e8b47fcbd
dijneal <133080518+dijneal@users.noreply.github.com>
2023-06-07 20:37:15 -0700

    update city names and ids for select. Added local timezone for select cities by creating function.

--- javascript/index.js
+++ javascript/index.js
@@ -22,3 +22,24 @@ function newYorkTime() {
 }
 newYorkTime();
 setInterval(newYorkTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format("hh:mm:ss")}
+          <small> ${cityTime.format("A")} </small>
+          </div>
+        </div>`;
+}
+
+let selectCity = document.querySelector("#select-cities");
+selectCity.addEventListener("change", updateCity);
